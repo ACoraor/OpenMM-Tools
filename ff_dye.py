fe5bd22461d73533320e9739e98edc9ba61508a7
Aron Edward Coraor <coraor@midway3-login4.rcc.local>
2023-07-18 13:54:17 -0500

    Filled out stubs for ff_dye.

--- ff_dye.py
+++ ff_dye.py
@@ -5,204 +5,200 @@
 import copy
 import numpy as np
 import mdtraj as md
+import pandas as pd
 import argparse
 import warnings
 import os
+import xml.etree.ElementTree as ET
+from xml.dom import minidom
 
 MASS = {"C":12.0107, "N": 14.0067, "O": 15.999, "P": 30.973762}
 
 def main():
-    """Modify the existing Cy3 and Cy5 pdbs to have a phosphate linker,
-    for direct inclusion in openMM structure. Phosphate group is coming
-    from the 5' side.
-
-    Truncate the lysine linkage and add an additional alkyl linkage.
+    """Create OpenMM Forcefield files based on the ff files from AMBER
+    dyes.
     """
+    #Load basic pdb
+    cy3_amber_top, cy5_amber_top = load_amber_pdbs(args.donor,args.acceptor)
+
+    #Load new pdbs
+    cy3_top, cy5_top = load_label_pdbs()
+
+    #Load AMBER-dyes forcefield
+    amber_dyes = load_amber_ffs(args.ff,cy3_amber_top,cy5_amber_top,
+        cy3_top,cy5_top)
     
-    #Load input pdb
-    cy3 = md.load_pdb(args.donor)
-    cy3_xyz = cy3.xyz[0]
-    cy3_top = cy3.top
-
-    cy5 = md.load_pdb(args.acceptor)
-    cy5_xyz = cy5.xyz[0]
-    cy5_top = cy5.top
-
-    print("PDBs loaded. Modifying...")
-
-    #Remove lysine tail
-    cy3_nt_xyz, cy3_nt_top = remove_tail(cy3_xyz,cy3_top)
-    cy5_nt_xyz, cy5_nt_top = remove_tail(cy5_xyz,cy5_top)
-
-    #Add phosphate and new alkyl linker
-    cy3_built_xyz, cy3_built_top = add_links(cy3_nt_xyz,cy3_nt_top)
-    cy5_built_xyz, cy5_built_top = add_links(cy5_nt_xyz,cy5_nt_top)
-
-    #Save new pdbs
-    pdb = md.formats.PDBTrajectoryFile('cy3_dna.pdb',mode='w')
-    pdb.write(10.*cy3_built_xyz,cy3_built_top)
-    
-    pdb = md.formats.PDBTrajectoryFile('cy5_dna.pdb',mode='w')
-    pdb.write(10.*cy5_built_xyz,cy5_built_top)
+    #Write ff xml
+    write_ff_xml(amber_dyes,cy3_top,cy5_top,cy3_amber_top,cy5_amber_top)
 
     print("DNA dye pdbs written! Exiting...")
 
 
-def remove_tail(xyz,top):
-    """Remove lysine tail from pdb.
+def load_amber_ffs(dye_path,cy3_amber_top,cy5_amber_top,cy3_top,cy5_top):
+    """Load AMBER-dyes gromacs-style forcefield file.
 
     Parameters:
-        xyz: *np.array*, shape: (n_atoms,3)
-            Positions of particles in the raw AMBER dye.
-        top: *mdtraj.topology*
-            Topology of the amber dye.
+        dye_path: *str*
+            Path to AMBER dye parameter root directory, typically
+            amber99sb_dyes.ff.
+        cy3_amber_top: *md.Topology*
+            Loaded Cy3_L1N Topology.
+        cy5_amber_top: *md.Topology*
+            Loaded Cy5_L1N Topology.
+        cy3_top: *md.Topology*
+            Loaded cy3 label topology.
+        cy5_top: *md.Topology*
+            Loaded cy5 label topology.
     Returns:
-        nt_xyz: *np.array*, shape: (n_atoms,3)
-            Positions of particles in the new AMBER dye without tail atoms.
-        nt_top: *mdtraj.topology*
-            A new Topology of the amber dye without tail atoms.
+        amber_charges: *list of pd.DataFrame*, shape: 2
+            DataFrame mapping atom names to charges for amber donor
+            acceptor
+        amber_dyes: *list of str*, shape: ????
+            Parsed AMBER-dyes parameters. Details undescribed.
     """
-    #Remove lysine
-    res = [r for r in top.residues]
+    #Notes from AMBER-dyes:
+    #Charge parameters and residue atom information can be found in the 
+    #amber99sb_dyes.ff/aminoacids.rtp.
+    #Bonded and nonbonded parameters are located in 
+    #amber99sb_dyes.ff/ffbonded.itp and amber99sb_dyes.ff/ffnonbonded.itp, 
+    #respectively.
+    #Atom types are located in amber99sb_dyes.ff/atomtypes.atp
 
-    rm_inds = []
-    #Remove all lysine atoms
-    for at in res[1].atoms:
-        rm_inds.append(at.index)
+    #aminoacids.arn: atom renaming."
+    #aminoacids.hdb: unparseable residue details."
+    #aminoacids.vsd: Virtual site details
+    #aminoacids.c.tdb and n.tdb: Empty
+    #aminoacids.r2b: Residue to buildingblock table?
 
-    rm_names = ["C25","C26","C99","O91", "H27","H28","H29","H30"]
-    for at in res[0].atoms:
-        if at.name in rm_names:
-            rm_inds.append(at.index)
+    #Create paths to parameter files
+    #charge_res_path = os.path.join(dye_path,'aminoacids.rtp')
+    cy3_rtp_path = os.path.join(dye_path,'cy3.rtp')
+    cy5_rtp_path = os.path.join(dye_path,'cy5.rtp')
+    bonded_params_path = os.path.join(dye_path,'ffbonded.itp')
+    nonbonded_params_path = os.path.join(dye_path,'ffnonbonded.itp')
+    atom_types_path = os.path.join(dye_path,'atomtypes.atp')
 
-    all_ats = [at for at in top.atoms]
-    keep_inds = [n for n in range(len(all_ats)) if n not in set(rm_inds)]
-    keep_inds = np.array(keep_inds,dtype=int)
+    #Get names of cy3, cy5 atoms
+    cy3_names = list(set([at.name for at in cy3_top.atoms]))
+    cy5_names = list(set([at.name for at in cy5_top.atoms]))
+    cy3_names.sort()
+    cy5_names.sort()
 
-    nt_xyz = xyz[keep_inds]
-    nt_top = top.subset(keep_inds)
+    #Read file text
+    with open(bonded_params_path,'r') as f:
+        bonded_params = f.readlines()
+    with open(nonbonded_params_path,'r') as f:
+        nonbonded_params = f.readlines()
+    with open(atom_types_path,'r') as f:
+        atom_types = f.readlines()
+   
+    #Parse amino acids for pandas
+    with open(cy3_rtp_path,'r') as f:
+        cy3_rtp = f.readlines()
+    with open(cy5_rtp_path,'r') as f:
+        cy5_rtp = f.readlines()
+   
+    #Manually parse the rtps
+    cy3_charges = cy3_rtp[5:74]
+    cy3_bonds = cy3_rtp[75:147]
+    cy3_impropers = cy3_rtp[148:167]
 
-    return nt_xyz,nt_top
+    cy5_charges = cy5_rtp[5:74]
+    cy5_bonds = cy5_rtp[75:147]
+    cy5_impropers = cy5_rtp[148:167]
+     
+
+    print("STUB!")
+    return dye_path
 
 
-def add_links(xyz,top):
-    """Add initial phosphate and terminal alkyl chain.
+def load_amber_pdbs(donor_path,acceptor_path):
+    """Load the pdbs at each path.
 
     Parameters:
-        xyz: *np.array*, shape: (n_atoms,3)
-            Positions of particles in the AMBER dye.
-        top: *mdtraj.topology*
-            Topology of the amber dye.
+        donor_path: *str*
+            Path to Cy3_L1N.pdb
+        acceptor_path: *str*
+            Path to Cy5_L1N.pdb
     Returns:
-        built_xyz: *np.array*, shape: (n_atoms,3)
-            Positions of particles in the new AMBER dye with linkers.
-        built_top: *mdtraj.topology*
-            A new Topology of the amber dye with linkers.
+        cy3_amber_top: *md.Topology*
+            Loaded Cy3_L1N Topology.
+        cy5_amber_top: *md.Topology*
+            Loaded Cy5_L1N Topology.
     """
-    res1 = [r for r in top.residues][0]
+    cy3 = md.load_pdb(args.donor)
+    cy5 = md.load_pdb(args.acceptor)
+
+    return cy3.top, cy5.top
+
+
+def load_label_pdbs(donor_path="cy3_dna.pdb",acceptor_path="cy5_dna.pdb"):
+    """Load the processed dna label pdbs at each path.
+
+    Parameters:
+        donor_path: *str*
+            Path to label-processed cy3 dye. Default: cy3_dna.pdb
+        acceptor_path: *str*
+            Path to label-processed cy5 dye. Default: cy5_dna.pdb
+    Returns:
+        cy3_top: *md.Topology*
+            Loaded cy3 label topology.
+        cy5_top: *md.Topology*
+            Loaded cy5 label topology.
+    """
+    cy3 = md.load_pdb(args.donor)
+    cy5 = md.load_pdb(args.acceptor)
+
+    return cy3.top, cy5.top
+
+def write_ff_xml(amber_dyes,cy3_top,cy5_top,cy3_amber_top,
+        cy5_amber_top,out='dyes.xml'):
+    """Create OpenMM-formatted forcefield file based on AMBER-dyes
+    parameters for the cy3 and cy5 residues.
+
+    Parameters:
+        amber_dyes: *list of str*, shape: ????
+            Parsed AMBER-dyes parameters. Details undescribed.
+        cy3_top: *md.Topology*
+            Loaded cy3 label topology.
+        cy5_top: *md.Topology*
+            Loaded cy5 label topology.
+        cy3_amber_top: *md.Topology*
+            Loaded Cy3_L1N Topology.
+        cy5_amber_top: *md.Topology*
+            Loaded Cy5_L1N Topology.
+        out: *str*
+            Path to outputfile. Default: 'dyes.xml'
+    """
+    xml_shell = make_xml_shell()
+
+    #Populate xml_shell
+    print("STUB!")
+
+
+
+def make_xml_shell():
+    """Create empty xml file in OpenMM Forcefield format.
+
+    Returns:
+        xml_shell: *xml.etree.ElementTree*
+            Empty ff file with appropriate tags.
+    """
+    print("STUB!")
+    root = minidom.Document()
+    ff = root.createElement('ForceField')
+    root.appendChild(ff)
+
+    atom_types = root.createElement('AtomTypes')
+    residues = root.createElement('Residues')
+    ff.appendChild(atom_types)
+    ff.appendChild(residues)
+
     
-    #Construct phosphate
-    sym = "P"
-    top.add_atom(sym,md.element.Element.getBySymbol(sym),residue=res1)
-
-    sym = "O"
-    top.add_atom("O3'",md.element.Element.getBySymbol(sym),residue=res1)
-    sym = "O"
-    top.add_atom("OP1",md.element.Element.getBySymbol(sym),residue=res1)
-    sym = "O"
-    top.add_atom("OP2",md.element.Element.getBySymbol(sym),residue=res1)
-
-    #Construct phosphate positions
-    #Treat the vector from C to O as the same as the prior leg
-    c0_i = top.select('name C24')[0]
-    c1_i = top.select('name C23')[0]
-    c2_i = top.select('name C22')[0]
-    #print("Indices:",c0_i,c1_i,c2_i)
-
-    #Works for 
-    C0 = xyz[c0_i]
-    C1 = xyz[c1_i]
-    C2 = xyz[c2_i]
-    
-    d1 = C1 - C2
-    d2 = C0 - C1
-    o3 = C0 + d1 #Oxygen 1 position
-    P = o3 + d2 #Phosphorus core position
-
-    d3 = d2 - d1 # Bonus vector to place OP3
-    h1_i = top.select('name H25')[0]
-    h2_i = top.select('name H26')[0]
-    H1 = xyz[h1_i]
-    H2 = xyz[h2_i]
-    dh3 = H1 - C0
-    dh4 = H2 - C0
-    op1 = P + 1.5*dh3
-    op2 = P + 1.5*dh4
-    
-
-
-    new_xyz = np.stack((P,o3,op1,op2))
-    xyz = np.concatenate((xyz,new_xyz))
-
-
-    #Add alkyl linker
-    #Remove hydrogens from other side
-    c2p_i = top.select('name C21')[0]
-    h0p_i = top.select('name H18')[0]
-    h1p_i = top.select('name H19')[0]
-    h2p_i = top.select('name H20')[0]
-
-    #Move H18, H19, prep to delete H20
-    c2_h1 = top.select('name H21')[0]
-    c2_h2 = top.select('name H22')[0]
-    d1p = xyz[c2_h1] - C2
-    d2p = xyz[c2_h2] - C2
-
-    xyz[h0p_i] = xyz[c2p_i] + d1p
-    xyz[h1p_i] = xyz[c2p_i] + d2p
-
-
-
-
-    #Add carbon linkers
-    sym = "C"
-    top.add_atom("CG",md.element.Element.getBySymbol(sym),residue=res1)
-    sym = "H"
-    top.add_atom("HG",md.element.Element.getBySymbol(sym),residue=res1)
-    sym = "H"
-    top.add_atom("HI",md.element.Element.getBySymbol(sym),residue=res1)
-    sym = "C"
-    top.add_atom("CI",md.element.Element.getBySymbol(sym),residue=res1)
-    sym = "H"
-    top.add_atom("HN",md.element.Element.getBySymbol(sym),residue=res1)
-    sym = "H"
-    top.add_atom("HN",md.element.Element.getBySymbol(sym),residue=res1)
-    sym = "O"
-    top.add_atom("O5'",md.element.Element.getBySymbol(sym),residue=res1)
-
-    #All carbons added
-    C1p = xyz[c2p_i]+d1
-    C0p = C1p + d2
-
-    #Add hydrogens
-    h1p1 = C1p - d1p
-    h2p1 = C1p - d2p
-    h1p2 = C0p + d1p
-    h2p2 = C0p + d2p
-    Op = C0p + d1
-
-
-    new_xyz = np.stack((C1p,h1p1,h2p1,C0p,h1p2,h2p2,Op))
-    xyz = np.concatenate((xyz,new_xyz))
-
-
-    xyz = np.concatenate((xyz[:h2p_i],xyz[h2p_i+1:]))
-    ats = [at for at in top.atoms]
-    inds = [i for i in range(len(ats)) if i != h2p_i]
-    top = top.subset(inds)
-
-    return xyz, top
-
+    xml_str = root.toprettyxml(indent='\t')
+    with open("test.xml",'w') as f:
+        f.write(xml_str)
+    return root    
 
 def foo(bar):
     """Init.
@@ -220,6 +216,8 @@ def foo(bar):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('-f','--ff', type=str, 
+        default='ff', help='Path to AMBER-dyes forcefield file.')
     parser.add_argument('-d','--donor', type=str, 
         default='C3N_L1R.pdb', help='Path to basic Cy3 pdb.')
     parser.add_argument('-a','--acceptor', type=str, 
