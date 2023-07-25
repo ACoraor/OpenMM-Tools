7d4211db5d27c89edd1b259e01a14db094e13e21
Aron Edward Coraor <coraor@midway3-login4.rcc.local>
2023-07-25 13:11:12 -0500

    Completed preliminary gaff-aec.xml generation.

--- ff_dye.py
+++ ff_dye.py
@@ -8,6 +8,7 @@ import mdtraj as md
 import pandas as pd
 import argparse
 import warnings
+import datetime
 import os
 from io import StringIO
 import xml.etree.ElementTree as ET
@@ -27,11 +28,12 @@ def main():
     cy3_top, cy5_top = load_label_pdbs()
 
     #Load AMBER-dyes forcefield
-    amber_dyes = load_amber_ffs(args.ff,cy3_amber_top,cy5_amber_top,
-        cy3_top,cy5_top)
+    amber_charges, amber_bonds, amber_impropers = load_amber_ffs(args.ff
+        ,cy3_amber_top,cy5_amber_top,cy3_top,cy5_top)
     
     #Write ff xml
-    write_ff_xml(amber_dyes,cy3_top,cy5_top,cy3_amber_top,cy5_amber_top)
+    write_ff_xml(amber_charges,amber_bonds,amber_impropers,cy3_top,cy5_top,
+        cy3_amber_top,cy5_amber_top,args.gaff,args.output)
 
     print("DNA dye pdbs written! Exiting...")
 
@@ -53,10 +55,12 @@ def load_amber_ffs(dye_path,cy3_amber_top,cy5_amber_top,cy3_top,cy5_top):
             Loaded cy5 label topology.
     Returns:
         amber_charges: *list of pd.DataFrame*, shape: 2
-            DataFrame mapping atom names to charges for amber donor
-            acceptor
-        amber_dyes: *list of str*, shape: ????
-            Parsed AMBER-dyes parameters. Details undescribed.
+            DataFrame mapping atom names to GAFF classes & charges for 
+            amber donor, acceptor
+        amber_bonds: *list of pd.DataFrame*, shape: 2
+            DataFrame with bonds between adjacent atoms.
+        amber_impropers: *list of pd.DataFrame*, shape: 2
+            DataFrame with improper dihedrals.
     """
     #Notes from AMBER-dyes:
     #Charge parameters and residue atom information can be found in the 
@@ -77,11 +81,6 @@ def load_amber_ffs(dye_path,cy3_amber_top,cy5_amber_top,cy3_top,cy5_top):
     cy3_rtp_path = os.path.join(dye_path,'cy3.rtp')
     cy5_rtp_path = os.path.join(dye_path,'cy5.rtp')
 
-    #Get names of cy3, cy5 atoms
-    cy3_names = list(set([at.name for at in cy3_top.atoms]))
-    cy5_names = list(set([at.name for at in cy5_top.atoms]))
-    cy3_names.sort()
-    cy5_names.sort()
 
    
     #Parse amino acids for pandas
@@ -165,8 +164,8 @@ def load_amber_ffs(dye_path,cy3_amber_top,cy5_amber_top,cy3_top,cy5_top):
     cy5_improper_df['a4'] = cy5_improper_a4
     cy5_improper_df.to_csv('cy5_improper.csv',index=False)
 
-    print("STUB!")
-    return dye_path
+    return ([cy3_charge_df,cy5_charge_df], [cy3_bond_df,cy5_bond_df],
+        [cy3_improper_df, cy5_improper_df])
 
 
 def load_amber_pdbs(donor_path,acceptor_path):
@@ -203,19 +202,24 @@ def load_label_pdbs(donor_path="cy3_dna.pdb",acceptor_path="cy5_dna.pdb"):
         cy5_top: *md.Topology*
             Loaded cy5 label topology.
     """
-    cy3 = md.load_pdb(args.donor)
-    cy5 = md.load_pdb(args.acceptor)
+    cy3 = md.load_pdb(donor_path)
+    cy5 = md.load_pdb(acceptor_path)
 
     return cy3.top, cy5.top
 
-def write_ff_xml(amber_dyes,cy3_top,cy5_top,cy3_amber_top,
-        cy5_amber_top,out='dyes.xml'):
+def write_ff_xml(amber_charges,amber_bonds,amber_impropers,cy3_top,cy5_top,
+        cy3_amber_top,cy5_amber_top,gaff,output):
     """Create OpenMM-formatted forcefield file based on AMBER-dyes
     parameters for the cy3 and cy5 residues.
 
     Parameters:
-        amber_dyes: *list of str*, shape: ????
-            Parsed AMBER-dyes parameters. Details undescribed.
+        amber_charges: *list of pd.DataFrame*, shape: 2
+            DataFrame mapping atom names to GAFF classes & charges for 
+            amber donor, acceptor
+        amber_bonds: *list of pd.DataFrame*, shape: 2
+            DataFrame with bonds between adjacent atoms.
+        amber_impropers: *list of pd.DataFrame*, shape: 2
+            DataFrame with improper dihedrals.
         cy3_top: *md.Topology*
             Loaded cy3 label topology.
         cy5_top: *md.Topology*
@@ -224,38 +228,130 @@ def write_ff_xml(amber_dyes,cy3_top,cy5_top,cy3_amber_top,
             Loaded Cy3_L1N Topology.
         cy5_amber_top: *md.Topology*
             Loaded Cy5_L1N Topology.
-        out: *str*
-            Path to outputfile. Default: 'dyes.xml'
+        gaff: *str*
+            Path to GAFF xml.
+        output: *str*
+            Path to output xml.
     """
-    xml_shell = make_xml_shell()
-
-    #Populate xml_shell
-    print("STUB!")
-
-
-
-def make_xml_shell():
-    """Create empty xml file in OpenMM Forcefield format.
-
-    Returns:
-        xml_shell: *xml.etree.ElementTree*
-            Empty ff file with appropriate tags.
-    """
-    print("STUB!")
-    root = minidom.Document()
-    ff = root.createElement('ForceField')
-    root.appendChild(ff)
-
-    atom_types = root.createElement('AtomTypes')
-    residues = root.createElement('Residues')
-    ff.appendChild(atom_types)
-    ff.appendChild(residues)
-
+    #Read gaff xml
+    xml = ET.parse(gaff)
+    xml_root = xml.getroot()    
     
-    xml_str = root.toprettyxml(indent='\t')
-    with open("test.xml",'w') as f:
+    #Get names of cy3, cy5 atoms
+    cy3_names = list(set([at.name for at in cy3_top.atoms]))
+    cy5_names = list(set([at.name for at in cy5_top.atoms]))
+    cy3_names.sort()
+    cy5_names.sort()
+    
+    
+    #Create map from atom name to class, charge.
+    cy3_charges = amber_charges[0]
+    cy5_charges = amber_charges[1]
+    cy3_charge_dict = {}
+    for i in range(len(cy3_charges['atom1'])):
+        at1 = cy3_charges['atom1'][i]
+        at2 = cy3_charges['atom2'][i]
+        qres = cy3_charges['QqmmmRESP'][i]
+        cy3_charge_dict[at1] = [at2,qres]
+        
+    cy5_charge_dict = {}
+    for i in range(len(cy5_charges['atom1'])):
+        at1 = cy5_charges['atom1'][i]
+        at2 = cy5_charges['atom2'][i]
+        qres = cy5_charges['QqmmmRESP'][i]
+        cy5_charge_dict[at1] = [at2,qres]
+
+    #Create new xml
+    new_root = ET.Element('ForceField')
+    new_xml = ET.ElementTree(new_root)
+    info = ET.SubElement(new_root,'Info')
+    date_gen = ET.SubElement(info,'DateGenerated')
+    date_gen.set('date',str(datetime.date.today()))
+    atomtypes = ET.SubElement(new_root,'AtomTypes')
+    for name in cy3_names:
+        if name not in cy3_charge_dict:
+            if name[0] == "C" and int(name[1:]) > 85:
+                #Aliphatic carbon
+                aliph_carbon = ET.SubElement(atomtypes,'Type')
+                aliph_carbon.set('element','C')
+                aliph_carbon.set('name',name)
+                aliph_carbon.set('class','c3')
+                aliph_carbon.set('mass','12.01')
+                #aliph_carbon.set('charge','-0.073')
+            if name[0] == "H" and int(name[1:]) > 85:
+                #Assume hcg, q=0.0229
+                aliph_hydrogen = ET.SubElement(atomtypes,'Type')
+                aliph_hydrogen.set('element','H')
+                aliph_hydrogen.set('name',name)
+                aliph_hydrogen.set('class','hc')
+                aliph_hydrogen.set('mass','1.008')
+                #aliph_hydrogen.set('charge','0.0229')
+    res_root = ET.SubElement(new_root,'Residues')
+
+    #Create residue template
+    cy3_res = ET.SubElement(res_root,"Residue")
+    cy3_res.set('name','C3N')
+    cy5_res = ET.SubElement(res_root,'Residue')
+    cy5_res.set('name','C5N')
+
+    #Loop through Cy3, cy5, creating residue
+    for at in cy3_top.atoms:
+        new_at = ET.SubElement(cy3_res,"Atom")
+        new_at.set('name',at.name)
+        if at.name in cy3_charge_dict:
+            charge_data = cy3_charge_dict[at.name]
+        elif "C" in at.name:
+            charge_data = ['c3g','-0.073']
+        elif "H" in at.name:
+            charge_data = ['hcg','0.0229']
+        elif "O" in at.name:
+            charge_data = ['og','-0.6458']
+        elif "P" in at.name:
+            charge_data = ['DNA-Pg','1.1659']
+        new_at.set('type',charge_data[0][:-1])
+        new_at.set('charge', str(np.float32(charge_data[1])))
+    #Add external bonds
+    ext_p = ET.SubElement(cy3_res,'ExternalBond')
+    ext_p.set('atomName',"P")
+    ext_o = ET.SubElement(cy3_res,'ExternalBond')
+    ext_p.set('atomName',"O3'")
+
+    for at in cy5_top.atoms:
+        new_at = ET.SubElement(cy5_res,"Atom")
+        new_at.set('name',at.name)
+        if at.name in cy3_charge_dict:
+            charge_data = cy5_charge_dict[at.name]
+        elif "C" in at.name:
+            charge_data = ['c3g','-0.073']
+        elif "H" in at.name:
+            charge_data = ['hcg','0.0229']
+        elif "O" in at.name:
+            charge_data = ['og','-0.6458']
+        elif "P" in at.name:
+            charge_data = ['DNA-Pg','1.1659']
+        new_at.set('type',charge_data[0][:-1])
+        new_at.set('charge', str(np.float32(charge_data[1])))
+    #Add external bonds
+    ext_p = ET.SubElement(cy3_res,'ExternalBond')
+    ext_p.set('atomName',"P")
+    ext_o = ET.SubElement(cy3_res,'ExternalBond')
+    ext_p.set('atomName',"O3'")
+    #Populate xml_shell
+ 
+    basic_str = ET.tostring(new_root,encoding='unicode')
+    #with open(output,'w') as f:   
+    #    f.write(basic_str)
+    #basic_str = str(basic_str)#.replace('\n','')
+    reparsed = minidom.parseString(basic_str)
+    xml_str = reparsed.toprettyxml(newl='\n')
+    #Replace silly strings
+    xml_str = xml_str.replace('\n\t\t\n','\n\t\t')
+    xml_str = xml_str.replace('\n\t\t    \n\t\t','\n\t\t')
+    xml_str = xml_str.replace('\n\t\t    \n\t\t','\n\t\t')
+    xml_str = xml_str.replace('\n\t\t  \n\t','\n\t')
+    with open(output,'w') as f:
         f.write(xml_str)
-    return root    
+
 
 def foo(bar):
     """Init.
@@ -274,11 +370,15 @@ def foo(bar):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-f','--ff', type=str, 
-        default='ff', help='Path to AMBER-dyes forcefield file.')
+        default='amber_subset', help='Path to AMBER-dyes forcefield file.')
     parser.add_argument('-d','--donor', type=str, 
         default='C3N_L1R.pdb', help='Path to basic Cy3 pdb.')
     parser.add_argument('-a','--acceptor', type=str, 
         default='C5N_L1R.pdb', help='Path to basic Cy5 pdb.')
+    parser.add_argument('-g','--gaff',type=str,default = "gaff.xml",
+	help="Path to GAFF parameter file.")
+    parser.add_argument('-o','--output',type=str,default = "gaff-aec.xml",
+	help="Path for output gaff xml.")
     args = parser.parse_args()
 
     main()
