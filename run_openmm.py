3057dfcc897e1455f3f8bd242d1bc6ed9b3ba741
Aron Edward Coraor <coraor@midway3-login3.rcc.local>
2024-02-26 18:05:21 -0600

    Add enhanced sampling and restarts.

--- run_openmm.py
+++ run_openmm.py
@@ -35,15 +35,33 @@ def main():
     #water = "spce_standard.xml"
     print("Starting system.")
 
+    
     #Load system
-    pdb, ff = load_system(args.pdb,args.amber,args.water,args.dyes,args.gaff)
+    if args.restart:
+        pdbfn = "init.pdb"
+    else:
+        pdbfn = args.pdb
+    pdb, ff = load_system(pdbfn,args.amber,args.water,args.dyes,args.gaff)
 
     #Create GAFF templates for OpenMM
     #pdb,ff = create_dye_templates(pdb,ff)    
 
     #Set up simulation environment
-    simulation = generate_simulation(pdb,ff)
+    if not args.restart:
+        simulation = generate_simulation(pdb,ff)
+    else:
+        T = 298.15 * unit.kelvin
+        dt = 2.0 * unit.femtoseconds
+        cutoff_distance = 1.0 * unit.nanometer
+        system = ff.createSystem(
+            pdb.topology, constraints = app.HBonds, 
+            nonbondedMethod = app.PME, nonbondedCutoff = cutoff_distance)
+        integrator = openmm.LangevinIntegrator(T, 1 / unit.picosecond, dt)
+        simulation = app.Simulation(pdb.topology, system, integrator)
     print("Simulation generated.")
+
+    #Add reporters
+    simulation = add_reporters(simulation)
 
     #Run the system
     print("Running simulation.")
@@ -61,10 +79,57 @@ def autogen_simulation():
 	simulation: *openmm.app.simulation*
 	    Preset simulation with langevin integrator.
     """
-    pdb,ff = load_system('cy3_isolated.pdb','DNA.bsc1.xml','spce_standard.xml',
-        'gaff-isolated.xml','gaff.xml')
-    simulation = generate_simulation(pdb,ff)
+    #pdb,ff = load_system('input.pdb','DNA.bsc1.xml','spce_standard.xml',
+    #    'gaff-isolated.xml','gaff.xml')
+    pdb,ff = load_system('input.pdb','DNA.bsc1.xml','spce_standard.xml',
+        'gaff-aec.xml','gaff.xml')
+    try:
+        simulation = generate_simulation(pdb,ff,is_restart=True)
+    except:
+        print("Failed to restart.")
+        simulation = generate_simulation(pdb,ff)
     simulation.minimizeEnergy()
+    add_reporters(simulation)
+    return simulation
+
+def add_reporters(simulation,is_restart=False):
+    """Add structure and thermodynamic mdtraj reporters to simulation.
+
+    Parameters:
+	simulation: *openmm.app.simulation*
+	    Preset simulation with langevin integrator.
+	is_restart: *bool*
+	    Is this simulation a restart
+    Returns:
+	simulation: *openmm.app.simulation*
+	    Preset simulation with langevin integrator.
+    """
+    relevant_inds = np.arange(3171) # Manually counted)
+
+    #Set up reporters
+    if os.path.isfile('output.h5'):        
+        if is_restart:
+            print("Concatenating outputs...")
+            if not os.path.isfile('full_output.h5'):
+                copy2('output.h5','full_output.h5')
+            else:
+                out_data = md.load_hdf5('output.h5')
+                full_data = md.load_hdf5('full_output.h5')
+                total_xyz = np.concatenate((full_data._xyz,out_data._xyz),axis=0)
+                new_traj = md.Trajectory(total_xyz,full_data._topology)
+                new_traj.save_hdf5('full_output.h5',force_overwrite=True)
+        
+        #All existing save-worthy data is now saved at full_output.h5
+        os.remove('output.h5')
+    odf = md.reporters.HDF5Reporter('output.h5',5000,atomSubset=relevant_inds)
+    simulation.reporters.append(odf)
+
+    #Use try/finally to handle datafile closure
+    #try:
+    state_data_file = open('state_data.txt','a')
+    simulation.reporters.append(app.StateDataReporter(state_data_file,5000,
+            step=True,potentialEnergy=True,temperature=True))
+    
     return simulation
 
 def load_system(pdb,amber,water,dyes,gaff):
@@ -94,7 +159,7 @@ def load_system(pdb,amber,water,dyes,gaff):
     return pdb,ff
 
 
-def generate_simulation(pdb, ff):
+def generate_simulation(pdb, ff,is_restart=False):
     """Generate an openmm simulation.
 
     Parameters:
@@ -102,6 +167,8 @@ def generate_simulation(pdb, ff):
 	    Loaded DNA construct pdb
 	ff: *openmm.app.Forcefield*
 	    Loaded forcefield file.
+	is_restart: *bool*
+	    Is this simulation a restart
     Returns:
 	simulation: *openmm.app.simulation*
 	    Preset simulation with langevin integrator.
@@ -121,13 +188,14 @@ def generate_simulation(pdb, ff):
     #pos *= 0.1 # Convert from angstroms to nanometers
     
     #Create standard bonds
-    topology.loadBondDefinitions(args.amber)
+    amber = "DNA.bsc1.xml"
+    topology.loadBondDefinitions(amber)#args.amber)
     topology.createStandardBonds()
     
     #Manually add bonds for C3N, C5N residues
     rs = [res for res in topology.residues()]
 
-    dye_names = ["C3I","C5I"]
+    dye_names = ["C3N","C5N"]
     dye_res = [r for r in rs if r.name in dye_names]
     print("dye_res:",dye_res)
 
@@ -186,7 +254,7 @@ def generate_simulation(pdb, ff):
                     float(pos[index_j].y),float(pos[index_j].z)]))
                 if np.linalg.norm(dist) < bond_dist:
                     topology.addBond(dye_ats[i],dye_ats[j])
-        '''
+        
         #Manually add bonds to the prior and later residue atoms
         dye_phos = [at for at in dye_ats if at.name=="P"][0]
         dye_O3 = [at for at in dye_ats if at.name=="O3'"][0]
@@ -198,7 +266,7 @@ def generate_simulation(pdb, ff):
         next_res = rs[dye_r.index+1]
         next_res_phos = [at for at in next_res.atoms() if at.name=="P"][0]
         topology.addBond(dye_O3,next_res_phos)
-        '''
+        
 
     
     #res = [r for r in topology.residues()]
@@ -206,9 +274,9 @@ def generate_simulation(pdb, ff):
     #Add spce solvent molecules
     modeller = app.modeller.Modeller(topology,pos)
     
-    bounds = Vec3(5.0,5.0,5.0)*unit.nanometer #Change
+    bounds = Vec3(32.0,5.0,5.0)*unit.nanometer #Change
     #dna_ff = app.ForceField(args.amber) 
-    modeller.loadHydrogenDefinitions(args.amber)
+    modeller.loadHydrogenDefinitions(amber)
     #print("Adding hydrogens...")
     modeller.addHydrogens(ff)
     print("Adding water...")
@@ -225,14 +293,24 @@ def generate_simulation(pdb, ff):
         nonbondedMethod = app.PME, nonbondedCutoff = cutoff_distance)
    
     #NPT at 1bar
-    system.addForce(MonteCarloBarostat(1*unit.bar,T)) 
+    system.addForce(MonteCarloAnisotropicBarostat(Vec3(1,1,1)*unit.bar,T)) 
 
     #Create Simulation
 
     integrator = openmm.LangevinIntegrator(T, 1 / unit.picosecond, dt)
-
-    simulation = app.Simulation(modeller.topology, system, integrator)
-    simulation.context.setPositions(modeller.positions)
+    
+    #STUB
+    is_restart=False
+    if is_restart:
+        try:
+            simulation = app.Simulation(modeller.topology, system, integrator,
+                state='restart_1.xml')
+        except:
+            simulation = app.Simulation(modeller.topology, system, integrator,
+                state='restart_0.xml')
+    else:
+        simulation = app.Simulation(modeller.topology, system, integrator)
+        simulation.context.setPositions(modeller.positions)
 
     #Set PBC ?
     #a = Vec3(10.0,0.0,0.0)
@@ -261,12 +339,11 @@ def run_simulation(simulation,n_ts=10000,is_restart=False,is_enhanced=False):
 
     if is_restart:
         #Attempt to load restart 1, if failed, try 0
+        print("Restarting simulation...")
         try:
-            simulation.loadState('restart_1.xml')
-            print("Successfully loaded restart_1.xml.")
+            simulation.loadCheckpoint("restart_1.xml")
         except:
-            simulation.loadState('restart_0.xml')
-            print("Successfully loaded restart_0.xml.")
+            simulation.loadCheckpoint("restart_0.xml")
     else:
         #Minimize initial condition
         print("Minimizing initial energy")
@@ -283,35 +360,6 @@ def run_simulation(simulation,n_ts=10000,is_restart=False,is_enhanced=False):
     simulation.reporters.append(app.StateDataReporter(sys.stdout,5000,
         step=True,potentialEnergy=True,temperature=True))
     
-    relevant_inds = np.arange(4205) # Manually counted)
-    
-    #Set up reporters
-    if os.path.isfile('output.h5'):        
-        if is_restart:
-            print("Concatenating outputs...")
-            if not os.path.isfile('full_output.h5'):
-                copy2('output.h5','full_output.h5')
-            else:
-                out_data = md.load_hdf5('output.h5')
-                full_data = md.load_hdf5('full_output.h5')
-                total_xyz = np.concatenate((full_data._xyz,out_data._xyz),axis=0)
-                new_traj = md.Trajectory(total_xyz,full_data._topology)
-                new_traj.save_hdf5('full_output.h5',force_overwrite=True)
-        
-        #All existing save-worthy data is now saved at full_output.h5
-        os.remove('output.h5')
-    simulation.reporters.append(md.reporters.HDF5Reporter('output.h5',5000,
-        atomSubset=relevant_inds))
-
-
-
-
-    #Use try/finally to handle datafile closure
-    #try:
-    state_data_file = open('state_data.txt','a')
-    simulation.reporters.append(app.StateDataReporter(state_data_file,5000,
-            step=True,potentialEnergy=True,temperature=True))
-    
     #Set up pysages cvs
     cvs = [DihedralAngle([26,27,28,22]),DihedralAngle([27,28,22,11])]
     grid = pysages.Grid(lower=(-np.pi, -np.pi), upper=(np.pi, np.pi), shape=(64, 64), periodic=True)
@@ -320,31 +368,45 @@ def run_simulation(simulation,n_ts=10000,is_restart=False,is_enhanced=False):
     
     
     #Run simulation
-    print("Running!")
-    if is_enhanced:
-        run_result = pysages.run(method,autogen_simulation,n_ts)
-        print("Simulation completed! Dumping...")
-       
-        with open("run_res.pickle",'wb') as f:
-            pickle.dump(run_result,f) 
-        print("Pysages data dumped to 'run_res.pickle'. Analyzing...")
-        return simulation
+    try:
+        print("Running!")
+        if is_enhanced:
+            # Create histogram logger
+            histogram_logger = HistogramLogger(period=100)
 
-    #Old style running:
-    for i in range(n_ts//traj_int):
-        simulation.step(traj_int)
-        pdb_fn = 'structure_%s.pdb' % i
-        #app.PDBFile.writeFile(simulation.topology,simulation.getpositions,pdb_fn) 
-        #print("simulation.topology:",simulation.topology())
-        #print("simulation positions:",simulation.getPositions())
-        
-        fn = "restart_%s.xml" % (i % 2)
-        with open(fn,'w') as f:
-            simulation.saveState(f)
+            run_result = pysages.run(method,autogen_simulation,n_ts,
+                    callback=histogram_logger)
+            print("Simulation completed! Dumping...")
+           
+            with open("run_res.pickle",'wb') as f:
+                pickle.dump(run_result,f) 
+            with open("run_hist.pickle",'wb') as g:
+                pickle.dump(histogram_logger,g)
+            print("Pysages data dumped to 'run_res.pickle'.")
+            print("Histograms dumped to 'run_hist.pickle'.")
+            print("Analyzing...")
+            return simulation
+        else:
+            #Old style running:
+            for i in range(n_ts//traj_int):
+                simulation.step(traj_int)
+                pdb_fn = 'structure_%s.pdb' % i
+                #app.PDBFile.writeFile(simulation.topology,simulation.getpositions,pdb_fn) 
+                #print("simulation.topology:",simulation.topology())
+                #print("simulation positions:",simulation.getPositions())
+                
+                fn = "restart_%s.xml" % (i % 2)
+                with open(fn,'wb') as f:
+                    simulation.saveCheckpoint(f)
+    finally:
+        for reporter in simulation.reporters:
+            try:
+                reporter.file.close()
+            except:
+                print("Couldn't close reporter %s" % str(reporter))
     
     print("Finished!")
     return simulation
-
 def plot_data(pickle_fn='run_res.pickle'):
     """Plot the output of a crashed pysages run.
 
@@ -355,7 +417,7 @@ def plot_data(pickle_fn='run_res.pickle'):
     with open(pickle_fn,'rb') as f:
         res = pickle.load(f)
     print("Training on data...")
-    result = pysages.analyze(res,topology=(14,))
+    result = pysages.analyze(res)
     print("Network trained.")
     A = result['free_energy']
     A = A.max() - A#A.max() - A
@@ -366,15 +428,16 @@ def plot_data(pickle_fn='run_res.pickle'):
     else:
         print("Type of A:",type(A))
     fig, ax = plt.subplots(dpi=120)
-    A_kT = A*1.688/4.184
+    A_kT = A/(2.479) # Convert from kJ/mol to kT
+    A = A_kT
     im = ax.imshow(
         A, interpolation="bicubic", origin="lower", 
             extent=[-np.pi, np.pi, -np.pi, np.pi])
     ax.contour(
-        A, levels=list(range(int(A.max()))), linewidths=0.75, colors="k", 
+        A, levels=list(range(0,int(A.max()),10)), linewidths=0.75, colors="k", 
             extent=[-np.pi, np.pi, -np.pi, np.pi])
     ax.set_xlabel(r"$\phi_2$ (rad)")
-    ax.set_ylabel(r"$\phi_3$ (rad)")
+    ax.set_ylabel(r"$\phi_1$ (rad)")
 
     cbar = plt.colorbar(im)
     cbar.ax.set_ylabel(r"$A~[kT]$", rotation=270, labelpad=20)
@@ -385,7 +448,44 @@ def plot_data(pickle_fn='run_res.pickle'):
     #plt.set_xlabel("$\\phi_3$ (deg)")
     plt.savefig("free_eng.pdf")
     plt.savefig("free_eng.png",dpi=600)
+    save_energy_forces(result)
+    plot_histogram(result)
+    plot_forces(result)
 
+def save_energy_forces(result):
+    """Energy/force saving function from Ludwig/Pablo
+
+    Parameters:
+	result: *pysages.analyze.result*
+	    Result of analyzed pysages simulation.
+    """
+    energy = numpy.asarray(result["free_energy"])
+    forces = numpy.asarray(result["mean_force"])
+    grid = numpy.asarray(result["mesh"])
+    numpy.savetxt("FES.csv", numpy.hstack([grid, energy.reshape(-1, 1)]))
+    numpy.savetxt("Forces.csv", numpy.hstack([grid, forces.reshape(-1, grid.shape[1])]))
+
+def plot_histogram(result):
+    surface = numpy.asarray(result["histogram"]) / numpy.nanmax(numpy.asarray(result["histogram"]))
+    fig, ax = plt.subplots()
+    im = ax.imshow(
+        surface, interpolation="bicubic", origin="lower", extent=[-np.pi, np.pi, -np.pi, np.pi], aspect=1
+    )
+    ax.contour(surface, levels=15, linewidths=0.75, colors="k", extent=[-np.pi, np.pi, -np.pi, np.pi])
+    plt.colorbar(im)
+    fig.savefig("histogram.pdf")
+
+def plot_forces(result):
+    fig, ax = plt.subplots()
+
+    ax.set_xlabel("CV")
+    ax.set_ylabel("Forces $[\\epsilon]$")
+
+    forces = numpy.asarray(result["mean_force"])
+    x = numpy.asarray(result["mesh"])
+    plt.quiver(x, forces, width=(0.0002 * (x[x.shape[0] - 1, 0] - x[0, 0])), headwidth=3)
+
+    fig.savefig("forces.pdf")
 
 def foo():
     """Do foo.
@@ -411,7 +511,7 @@ if __name__ == '__main__':
 	help="Enhanced sampling simulation.")
     parser.add_argument('-w','--water',type=str,default = "spce_standard.xml",
 	help="Path to Water/SPCE parameter file.")
-    parser.add_argument('-d','--dyes',type=str,default = "gaff-isolated.xml",
+    parser.add_argument('-d','--dyes',type=str,default = "gaff-aec.xml",
 	help="Path to dyes parameter file.")
     parser.add_argument('-g','--gaff',type=str,default = "gaff.xml",
 	help="Path to GAFF parameter file.")
