e5d05e6f8491b6ae6c5b5c3286d0e8beb2f00de4
Aron Edward Coraor <coraor@midway3-login4.rcc.local>
2024-03-11 18:05:23 -0500

    Complete stub for rna_replace before debugging.

--- rna_replace.py
+++ rna_replace.py
@@ -21,17 +21,14 @@ def main():
     top = pdb.top
 
     #Load construct pdbs
-    d_pdb = md.load_pdb("cy3_dna.pdb")
-    d_xyz = d_pdb.xyz[0]
-    d_top = d_pdb.top
+    construct_pdb = md.load_pdb(args.label)
+    construct_xyz = construct_pdb.xyz[0]
+    construct_top = construct_pdb.top
+
     
-    a_pdb = md.load_pdb("cy5_dna.pdb")
-    a_xyz = a_pdb.xyz[0]
-    a_top = a_pdb.top
 
     #Add construct in the correct locations
-    xyz, top = add_dyes(xyz, top, d_xyz, d_top, a_xyz, a_top, args.donor,
-        args.acceptor)
+    xyz, top = add_dyes(xyz, top, construct_xyz,construct_top,args.start,args.end)
 
     #Save new pdb
     pdb = md.formats.PDBTrajectoryFile(args.output,mode='w')
@@ -39,50 +36,41 @@ def main():
 
     print("Labelled pdb written. Exiting...")
 
-def add_dyes(xyz, top, donor_xyz, donor_top, acceptor_xyz, acceptor_top,
-         donor_ind, acceptor_ind):
-    """Remove the appropriate atoms of the DNA residue to replace. Reorient
-    the donor and acceptor to the appropriate residue and add to the pdb.
+def add_dyes(xyz, top, construct_xyz, construct_top,start,end):
+    """Remove the appropriate atoms of the RNA residues to replace. Reorient
+    the construct to the appropriate positions and orientations
 
     Parameters:
         xyz: *np.array*, shape: (n_atoms,3)
             Positions of particles in the input DNA pdb.
         top: *mdtraj.topology*
             Topology of the input DNA pdb.
-        donor_xyz: *np.array*, shape: (n_atoms,3)
+        construct_xyz: *np.array*, shape: (n_atoms,3)
             Positions of particles in the Cy3 dye with linkers.
-        donor_top: *mdtraj.topology*
+        construct_top: *mdtraj.topology*
             Topology of the Cy3 dye with linkers.
-        acceptor_xyz: *np.array*, shape: (n_atoms,3)
-            Positions of particles in the Cy5 dye with linkers.
-        acceptor_top: *mdtraj.topology*
-            Topology of the Cy5 dye with linkers.
-        donor_ind: *int*
-            Index of the donor with respect to its strand, 5' direction.
-        acceptor_ind: *int*
-            Index of the acceptor with respect to its strand, 5' direction.
+        start: *int*
+            Index of residue start for label insertion
+        end: *int*
+            Index of residue end for label insertion
     Returns:
-        labelled_xyz: *np.array*, shape: (n_atoms,3)
-            Positions of particles in the labelled DNA pdb.
-        labelled_top: *mdtraj.topology*
-            Topology of the labelled DNA pdb.
+        xyz: *np.array*, shape: (n_atoms,3)
+            Positions of particles in the chimeric pdb.
+        top: *mdtraj.topology*
+            Topology of the Chimeric pdb.
     """
     #First: create a truncated version with all the appropriate residue 
     #Atoms removed 
-    trunc_xyz, trunc_top = remove_dna_atoms(xyz, top, args.donor, 
-        args.acceptor)
+    trunc_xyz, trunc_top = remove_rna_atoms(xyz, top, start, 
+        end)
 
-    #Next: get the inlet and outlet phosphorus and oxygen positions
-    d_phos_in, d_ox_in, d_ox_out, d_dna_com = get_phos_ox(xyz, top, 
-        donor_ind, is_donor=True) 
-    a_phos_in, a_ox_in, a_ox_out, a_dna_com = get_phos_ox(xyz, top, 
-        acceptor_ind, is_donor=False)
-
+    #Get inlet and outlet phosphorus positions
+    phos_in, c2_pos,o5_pos, phos_out, start_com = get_phos_rna(xyz, top, 
+        start,end) 
+    
     #Rotate and translate the dye molecules to line up the phosphates
-    affine_donor_xyz, affine_donor_top = affine_translate(d_phos_in,
-        d_ox_in, d_ox_out, donor_xyz, donor_top,d_dna_com)
-    affine_acceptor_xyz, affine_acceptor_top = affine_translate(a_phos_in,
-        a_ox_in, a_ox_out, acceptor_xyz,acceptor_top,a_dna_com)
+    affine_construct_xyz, affine_construct_top = affine_translate_rna(phos_in,
+        c2_pos,o5_pos, phos_out, construct_xyz, construct_top,start_com)
 
     #Combine xyzs and tops:
     #labelled_xyz = np.concatenate((trunc_xyz, affine_donor_xyz, affine_acceptor_xyz))
@@ -99,22 +87,15 @@ def add_dyes(xyz, top, donor_xyz, donor_top, acceptor_xyz, acceptor_top,
         curr_ch = labelled_top.chain(-1)
         for res_i, res in enumerate(chain.residues):
             #If you hit the dye residue, first add the dye
-            if chain_i == 0 and res_i == donor_ind:
-                donor_res = [r for r in affine_donor_top.residues][0]
-                labelled_top.add_residue(name='C3N',chain=curr_ch)
-                curr_res = labelled_top.residue(-1)
-                for d_at in donor_res.atoms:
-                    labelled_top.add_atom(name=d_at.name,
-                        element=d_at.element, residue=curr_res)
-                labelled_xyz += [affine_donor_xyz]
-            elif chain_i == 1 and res_i == acceptor_ind:
-                acc_res = [r for r in affine_acceptor_top.residues][0]
-                labelled_top.add_residue(name='C5N',chain=curr_ch)
-                curr_res = labelled_top.residue(-1)
-                for acc_at in acc_res.atoms:
-                    labelled_top.add_atom(name=acc_at.name,
-                        element=acc_at.element, residue=curr_res)
-                labelled_xyz += [affine_acceptor_xyz]
+            if chain_i == 0 and res_i == start:
+                donor_res = [r for r in affine_construct_top.residues]
+                for construct_res in donor_res:
+                    labelled_top.add_residue(name=construct_res.name,chain=curr_ch)
+                    curr_res = labelled_top.residue(-1)
+                    for d_at in construct_res.atoms:
+                        labelled_top.add_atom(name=d_at.name,element=d_at.element,
+                            residue = curr_res
+                labelled_xyz += [affine_construct_xyz]
             
             #Now, add the normal residue and atoms
             labelled_top.add_residue(name=res.name,chain=curr_ch)
@@ -156,7 +137,7 @@ def add_dyes(xyz, top, donor_xyz, donor_top, acceptor_xyz, acceptor_top,
 
     return labelled_xyz, labelled_top
 
-def remove_dna_atoms(xyz, top, donor_ind, acceptor_ind):
+def remove_rna_atoms(xyz, top, start, end):
     """Remove relevant atoms from each donor/acceptor residue in the
     pdb structure.
 
@@ -165,10 +146,10 @@ def remove_dna_atoms(xyz, top, donor_ind, acceptor_ind):
             Positions of particles in the input DNA pdb.
         top: *mdtraj.topology*
             Topology of the input DNA pdb.
-        donor_ind: *int*
-            Index of the donor with respect to its strand, 5' direction.
-        acceptor_ind: *int*
-            Index of the acceptor with respect to its strand, 5' direction.
+        start: *int*
+            Index of residue start for label insertion
+        end: *int*
+            Index of residue end for label insertion
     Returns:
         trunc_xyz: *np.array*, shape: (n_atoms,3)
             Positions of particles in the truncated DNA pdb.
@@ -181,14 +162,13 @@ def remove_dna_atoms(xyz, top, donor_ind, acceptor_ind):
 
     chains = [c for c in top.chains]
     ch1_res = [r for r in chains[0].residues]
-    donor_residue = ch1_res[donor_ind]
-    ch2_res = [r for r in chains[1].residues]
-    acceptor_residue = ch2_res[acceptor_ind]
-
-    for at in donor_residue.atoms:
-        rm_inds.append(at.index)
-    for at in acceptor_residue.atoms:
-        rm_inds.append(at.index)
+    #donor_residue = ch1_res[donor_ind]
+    #ch2_res = [r for r in chains[1].residues]
+    #acceptor_residue = ch2_res[acceptor_ind]
+    for rm_ind in range(start,end+1):
+        rm_res = ch1_res[rm_ind]
+        for at in rm_res.atoms:
+            rm_inds.append(at.index)
 
     keep_inds = [i for i in range(len(all_atoms)) if i not in set(rm_inds)]
     trunc_top = top.subset(keep_inds)
@@ -199,7 +179,7 @@ def remove_dna_atoms(xyz, top, donor_ind, acceptor_ind):
 
     return trunc_xyz, trunc_top
 
-def get_phos_ox(xyz, top,index, is_donor):
+def get_phos_rna(xyz, top,start,end):
     """Get the positions of the inlet and outlet phosphates and O?3? to 
     determine orientation of dyes.
 
@@ -208,18 +188,20 @@ def get_phos_ox(xyz, top,index, is_donor):
             Positions of particles in the input DNA pdb.
         top: *mdtraj.topology*
             Topology of the input DNA pdb.
-        index: *int*
-            Index of the dye with respect to its strand, 5' direction.
-        is_donor: *bool*
-            Whether or not this is on the donor or the acceptor strand.
+        start: *int*
+            Index of residue start for label insertion
+        end: *int*
+            Index of residue end for label insertion
     Returns:
-        phos_in: *np.array*, shape: (3)
-            Position of the inlet phosphorus from the 3' direction.
-        ox_in: *np.array*, shape: (3)
-            Position of the inlet O3' from the 3' direction.
-        ox_out: *np.array*, shape: (3)
-            Position of the outlet O5' from the 5' direction.
-        dna_com: *np.array*, shape: (3)
+        phos_st: *np.array*, shape: (3)
+            Position of the inlet phosphorus from the 5' direction.
+        c2_pos: *np.array*, shape: (3)
+            Position of the c2' carbon at start ind.
+        o5_pos: *np.array*, shape: (3)
+            Position of the O5' oxyen direction.
+        phos_out: *np.array*, shape: (3)
+            Position of the outlet P at end.
+        rna_com: *np.array*, shape: (3)
             Average position of the labelled DNA residue.
     """
     #We seek to exactly overlay the incoming 3' phosphate, and its OP1
@@ -227,14 +209,11 @@ def get_phos_ox(xyz, top,index, is_donor):
     #We rotate the molecule until this is matched, and then rotate until 
     #The outlet O5' position is matched.
     chains = [ch for ch in top.chains]
-    if is_donor:
-        chain = chains[0]
-    else:
-        chain = chains[1]
+    chain = chains[0]
 
     residues = [r for r in chain.residues]
     #outgoing_res = residues[index+1]
-    dye_res = residues[index]
+    dye_res = residues[start]
     #outgoing_atoms = [at for at in outgoing_res.atoms]
     dye_res_atoms = [at for at in dye_res.atoms]
     #for at in outgoing_atoms:
@@ -243,85 +222,110 @@ def get_phos_ox(xyz, top,index, is_donor):
     for at in dye_res_atoms:
         if at.name == "P":
             phos_in = xyz[at.index]
-        if at.name == "OP1":
-            ox_in = xyz[at.index]
-        elif at.name == "O3'":
-            ox_out = xyz[at.index]
+        if at.name == "O5'":
+            o5_pos = xyz[at.index]
+        if at.name == "C2'":
+            c2_pos = xyz[at.index]
 
-    dna_com = np.average(xyz[[at.index for at in dye_res_atoms]],axis=0)
+    #Get phos out
+    for at in [at for at in residues[end].atoms]
+        if at.name == "P":
+            phos_out = xyz[at.index]
 
-    return phos_in, ox_in, ox_out, dna_com
+    rna_com = np.average(xyz[[at.index for at in dye_res_atoms]],axis=0)
 
-def affine_translate(phos_in, ox_in, ox_out,dye_xyz,dye_top,dna_com):
+    return phos_in, c2_pos, o5_pos, phos_out,rna_com
+
+def affine_translate_rna(phos_in, c2_pos, o5_pos, phos_out,construct_xyz,construct_top,rna_com):
     """Rotate the dye until it aligns with the appropriate phosphate in/out
 
     Parameters:
         phos_in: *np.array*, shape: (3)
             Position of the inlet phosphorus from the 3' direction.
-        ox_in: *np.array*, shape: (3)
-            Position of the inlet OP1 from the 3' direction.
-        ox_out: *np.array*, shape: (3)
+        c2_pos: *np.array*, shape: (3)
+            Position of the c2' carbon at start ind.
+        o5_pos: *np.array*, shape: (3)
+            Position of the O5' oxyen direction.
+        phos_out: *np.array*, shape: (3)
             Position of the outlet OP2 from the 5' direction.
-        dye_xyz: *np.array*, shape: (n_atoms,3)
+        construct_xyz: *np.array*, shape: (n_atoms,3)
             Positions of particles in the dye with linkers.
-        dye_top: *mdtraj.topology*
+        construct_top: *mdtraj.topology*
             Topology of the dye pdb.
-        dna_com: *np.array*, shape: (3)
+        rna_com: *np.array*, shape: (3)
             Average position of the labelled DNA residue.
     Returns:
-        dye_xyz: *np.array*, shape: (n_atoms,3)
+        affine_xyz: *np.array*, shape: (n_atoms,3)
             Positions of particles in the rotated dye with linkers.
+        affine_top: *mdtraj.topology*
+            Topology of the dye pdb.
     """
     fvu0 = np.eye(3)
     quat0 = tu2rotquat(1e-5,np.array([1,0,0]))
-    quat = np.copy(quat0)
+    quat = np.copy(quat0) #Running accumulator of quaternion
 
-    #Translate dye to 0
-    dye_xyz = dye_xyz - np.average(dye_xyz,axis=0)
+    #Translate last opposing-strand phosphate to 0
+    conjugate_res = [res for res in construct_top.residues]
+    conjugate_inds = [at.index for at in conjugate_res[-1].atoms]
+    phos_ind = [at.index for at in conjugate_res[-1].atoms if at.name == "P"][0]
+
+    construct_xyz = construct_xyz - construct_xyz[phos_ind]
 
     #Get relevant dye atoms
-    dye_ats = [at for at in dye_top.atoms]
+    construct_ats = [at for at in conjugate_res.atoms]
     for at in dye_ats:
         if at.name == "P":
-            dye_P = dye_xyz[at.index]
+            construct_P = construct_xyz[at.index]
+            construct_phos_ind = at.index
+        elif at.name == "C2'":
+            construct_c2 = construct_xyz[at.index]
+            construct_c2_ind = at.index
         elif at.name == "O5'":
-            dye_o5 = dye_xyz[at.index]
-        elif at.name == "O3'":
-            dye_o3 = dye_xyz[at.index]
+            construct_o5 = construct_xyz[at.index]
+            construct_o5_ind = at.index
 
-    #Now: Calculate angle to rotate along PO vector to align O5
-    p_O3_vect = dye_o3 - dye_P
-    dna_p_O3_vect = ox_out - phos_in
-
-    #Project dna vector onto the plane of rotation
-    p_O3_vect /= np.linalg.norm(p_O3_vect)
-    dna_p_O3_vect /= np.linalg.norm(dna_p_O3_vect)
+    #Idea: Bring P --> C2, P --> O5, and C2 --> O5 vectors into alignment
+    p_c2_vect = construct_c2 - construct_P
+    dna_p_c2_vect = c2_pos - phos_in
+    axis_vect = np.cross(p_c2_vect,dna_p_c2_vect)
+    axis_vect /= np.linalg.norm(axis_vect)
+    first_angle = vector_angle(p_c2_vect,dna_p_c2_vect)
     
-    rot_axis = np.cross(p_O3_vect,dna_p_O3_vect)
-    rot_axis /= np.linalg.norm(rot_axis)
+    q = tu2rotquat(first_angle,axis_vect)
+    dye_xyz = np.array([quat_vec_rot(row, q) for row in construct_xyz])
+    #Now: P-->C2 axes are matched
 
-    dotp = np.dot(p_O3_vect,dna_p_O3_vect)
-    if np.abs(dotp) > 1:
-        dotp = dotp/np.abs(dotp)
-    theta = np.arccos(dotp)
-    q = tu2rotquat(theta,rot_axis)
+    #Bring P --> O5 vectors into alignment
+    construct_o5 = dye_xyz[construct_o5_ind]
+    construct_P = dye_xyz[construct_phos_ind]
+    construct_c2 = dye_xyz[construct_c2_ind]
+    p_o5_vect = construct_o5 - construct_P
+    dna_p_o5_vect = o5_pos - phos_in
+    axis_vect = dna_p_c2_vect#Rotate along dna_p_c2_vect
+    #Verify that dots are zero:
+    print("dna-axis dot (should be 0):",np.dot(dna_p_o5_vect,axis_vect))    
+    print("construct-axis dot (should be 0):",np.dot(p_o5_vect,axis_vect))    
+    second_angle = vector_angle(p_o5_vect,dna_p_o5_vect)
+    q = tu2rotquat(second_angle,axis_vect)
 
-    #Rotate dye into place   
     dye_xyz = np.array([quat_vec_rot(row, q) for row in dye_xyz])
-    dye_ats = [at for at in dye_top.atoms]
-    for at in dye_ats:
-        if at.name == "P":
-            dye_P = dye_xyz[at.index]
-        elif at.name == "O5'":
-            dye_o5 = dye_xyz[at.index]
-        elif at.name == "O3'":
-            dye_o3 = dye_xyz[at.index]
-    
-    p_O3_vect = dye_o3 - dye_P
-    p_O3_vect /= np.linalg.norm(p_O3_vect)
-    print("Final dot between p_O3 and DNA p_o3 vectors (should be 1):",
-            np.dot(p_O3_vect,dna_p_O3_vect))
+    construct_o5 = dye_xyz[construct_o5_ind]
+    construct_P = dye_xyz[construct_phos_ind]
+    construct_c2 = dye_xyz[construct_c2_ind]
 
+    c2_o5_vect = construct_o5 - construct_P
+    dna_c2_o5_vect = o5_pos - c2_pos
+
+ 
+    print("Final dot between c2_O5 and DNA c2_o5 vectors (should be ~1):",
+            np.dot(c2_o5_vect,dna_c2_o5_vect))
+    fn = "affine_construct.pdb"
+    pdb = md.formats.PDBTrajectoryFile(fn,mode='w')
+    pdb.write(10.*dye_xyz,construct_top)
+
+    return dye_xyz, construct_top
+
+    print("Unreachable code!")
     #Dye is now rotated so phos_in - ox_out axis is correct. now rotate
     #the COM to the outside.
     dye_com = np.average(dye_xyz,axis=0)
@@ -406,14 +410,16 @@ def foo(bar):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f','--file', type=str, default='input.pdb',
-        help='Path to PDB of ideal DNA structure, with correct endings.')
+    parser.add_argument('-f','--file', type=str, default='EGFP_key.pdb',
+        help='Path to PDB of ideal RNA structure.')
+    parser.add_argument('-l','--label', type=str, default='82bp_labelled.pdb',
+        help='Path to PDB of labelled DNA.')
     parser.add_argument('-o','--output', type=str, default='labelled_dna.pdb',
-        help='Path to output labelled PDB.')
-    parser.add_argument('-d','--donor', type=int, 
-        default=15, help='Index of Cy3 in donor (Ashort) strand. Default=15')
-    parser.add_argument('-a','--acceptor', type=int, 
-        default=23, help='Index of Cy5 in Acceptor (B) strand. Default=23 (B10)')
+        help='Path to output chimeric PDB.')
+    parser.add_argument('-s','--start', type=int, default=100,
+        help='Index of first removed RNA base, from 0')
+    parser.add_argument('-e','--end', type=int, default=111,
+        help='Index of last removed RNA base, from 0')
     args = parser.parse_args()
 
     main()
