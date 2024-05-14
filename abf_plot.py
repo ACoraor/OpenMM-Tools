a8b1c418e2c290f807d692594bc8bde0793e00f6
Aron Edward Coraor <coraor@midway3-0284.rcc.local>
2024-05-14 11:14:09 -0500

    Add 1D FES processing.

--- abf_plot.py
+++ abf_plot.py
@@ -15,8 +15,14 @@ import matplotlib.cm as cm
 def main():
     """Plot the raw ABF integration FES properly."""
     fes = np.loadtxt(args.file)
-
-    make_plot(fes,args.max)
+    
+    #Check for 1D FES
+    if fes.shape[1] == 3:
+        make_plot(fes,args.max)
+    elif fes.shape[1] == 2:
+        make_1d_plot(fes,args.max)
+    else:
+        raise Exception("Number of columns in %s can't be processed." % args.file)
     fn = 'abf_fes.png'
     print("Saved converted %s." % fn)
 
@@ -49,14 +55,14 @@ def make_plot(fes,max_eng=20):
 
     #Reshape into meshgrid for plotting
     XX = np.reshape(fes[:,0]*180.0/np.pi,(200,200))
-    YY = np.reshape(fes[:,1]*10.0,(200,200))
-    #YY = np.reshape(fes[:,1]*180.0/np.pi,(200,200))
+    #YY = np.reshape(fes[:,1]*10.0,(200,200))
+    YY = np.reshape(fes[:,1]*180.0/np.pi,(200,200))
     ZZ = np.reshape(energies,(200,200))
 
     #For soft_041: flip xx and yy
-    tmp = np.copy(XX)
-    XX = YY
-    YY = tmp
+    #tmp = np.copy(XX)
+    #XX = YY
+    #YY = tmp
 
     ax.contourf(XX,YY,ZZ,levels = np.arange(max_eng),
         norm=norm,cmap=cmap)
@@ -64,15 +70,40 @@ def make_plot(fes,max_eng=20):
         colors = "black",linewidths=0.5)
     cbar = plt.colorbar(mappable=sm,label = "Free energy (kT)",ax=ax,
         orientation='vertical')
-    #ax.set_xlabel("Backbone Angle (deg)")
+    ax.set_xlabel("Backbone Angle (deg)")
+    #ax.set_xlabel("Intercalation Angle (deg)") #soft_041
     #ax.set_ylabel("Intercalation distance ($\\AA$)")
-    ax.set_xlabel("Intercalation distance ($\\AA$)")
-    ax.set_ylabel(r"$\theta_{bending}$ (deg)")
+    #ax.set_xlabel("Intercalation distance ($\\AA$)")
+    ax.set_ylabel(r"$\theta_{bending}$ (deg)") #soft_041
     #ax.set_xlabel(r"$\theta_{bending}$ (deg)")
-    try:
-        plt.tight_layout()
-    except:
-        print("Error: cannot do tight_layout.")
+    plt.savefig('abf_fes.png',dpi=600)
+    plt.savefig('abf_fes.pdf')
+    plt.savefig('abf_fes.eps')
+
+
+def make_1d_plot(fes,max_eng=20):
+    '''Create a 1d fes plot.
+
+    Parameters:
+        fes: *np.array*, shape: (len(x)*len(y),3)
+            First two columns are x and y, last column is free energy
+                in kJ/mol
+        max_eng: *int or float*
+            Maximum energy to plot up to. Default = 20.
+    '''
+    
+    plt.clf()
+    fig,ax = plt.subplots(layout='constrained')
+    energies = fes[:,1]
+    energies -= np.min(energies)
+    energies /= 2.479 # Convert kJ/mol to kT
+    XX = fes[:,0]*180.0/np.pi
+    ZZ = energies
+
+    plt.plot(XX,ZZ)
+
+    ax.set_xlabel("Backbone Angle (deg)")
+    ax.set_ylabel(r"Free energy (kT)") #soft_041
     plt.savefig('abf_fes.png',dpi=600)
     plt.savefig('abf_fes.pdf')
     plt.savefig('abf_fes.eps')
@@ -81,7 +112,7 @@ def make_plot(fes,max_eng=20):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f','--file', type=str, default='output.h5',
+    parser.add_argument('-f','--file', type=str, default='abf_fes_integrated.txt',
         help='Path to ABF integrated output')
     parser.add_argument('-m','--max', type=float, default=40.0,
         help='Maximum free energy to plot up to. Default = 40.0.')
