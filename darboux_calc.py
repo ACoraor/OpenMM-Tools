a69f8e961567d84c9a9a338f5a5c3dcc9368a0d2
Aron Edward Coraor <coraor@midway3-login4.rcc.local>
2024-09-26 12:42:27 -0500

    Add Darboux calculation tools.

---
+++ darboux_calc.py
@@ -0,0 +1,333 @@
+# Module distance
+# Created by Aria Coraor, 4/29/24
+
+import numpy as np
+import os
+import sys
+
+# from merlin import files
+# from nucl_load import nucl_load,np_conv_lammps
+# from subtool import inb4
+import pandas as pd
+import mdtraj as md
+
+# from recalc import calc_re
+# import rvect_calc
+import argparse
+
+
+def main():
+    """Calculate the average Darboux vector distribution for C3N and C5N 
+    residues relative to the opposing and N+-1 basepairs."""
+
+    # Load hdf5s
+    if os.path.isfile('output.h5'):
+        out = md.load_hdf5("output.h5")
+    else:
+        out = None
+    
+    if os.path.isfile('full_output.h5'):
+        full_out = md.load_hdf5("full_output.h5")
+        if out is not None:
+            xyz = np.concatenate((full_out.xyz, out.xyz), axis=0)
+            top = full_out.topology
+        else:
+            xyz = full_out.xyz
+            top = full_out.topology
+
+    else:
+        xyz = out.xyz
+        top = out.topology
+
+    #Calculate the orientation of each dye residue in the Darboux frame
+    t_vects, names = calc_dye_darboux(xyz,top)
+
+    # Write to pandas dframe
+    save_to_dframe(t_vects,names)
+
+    print("Saved distances in %s. Exiting..." % os.getcwd())
+
+def save_to_dframe(t_vects,names):
+    """Save the Darboux orientation vectors to log.dframe
+
+    *Parameters*
+            angs:  *np.array of floats*, shape: (n_ts)
+                    Angle, in radians, of the fiber bending.
+    """
+    df = pd.read_csv("log.dframe")
+
+    #Loop through names and add each column of t_vects in
+    for i, name in enumerate(names):
+        df[name+"_t1"] = t_vects[i][:,0]
+        df[name+"_t2"] = t_vects[i][:,1]
+        df[name+"_t3"] = t_vects[i][:,2]
+    df.to_csv("log.dframe", index=False, na_rep="NULL")
+
+
+def calc_dye_darboux(xyz, top):
+    """Calculate the darboux orientation vector for each dye residue
+    in the topology for all frames.
+
+    *Parameters*
+            xyz:  *np.array of floats*, shape: (n_ts,n_atoms,3)
+                    Atomic positions.
+            top: *mdtraj.Topology*
+                    Topology of the system
+    *Returns*
+            t_vects: *list of np.array*, shape: [n_dyes,(n_ts,3)]
+                    Components of the dye's orientation in the Darboux frame.
+            names: *list of str*
+                    Names of the dyes.
+    """
+    
+    #Determine the residues of dyes
+    chains = [ch for ch in top.chains]
+    
+    cy3_res = []
+    cy5_res = []
+    residues = [res for res in top.residues]
+    for r in residues:
+        if r.name == "C3N":
+            cy3_res.append(r)
+        elif r.name == "C5N":
+            cy5_res.append(r)
+
+    all_dyes = cy3_res + cy5_res
+    dye_names = ["cy3_%s" % i for i in range(len(cy3_res))]+ [
+            "cy5_%s" % i for i in range(len(cy5_res))]
+
+
+    res_5p = [] #List of N+1 residues in the 5' direction
+    res_3p = [] #List of N-1 residues in the 3' direction
+    res_5p_opp = [] #List of N+1 residues complementary to the 5p
+    res_3p_opp = [] #List of N-1 residues complementary to the 3p
+
+    #Loop through dyes, identifying corresponding N+1 and N-1 residues
+    for i, dye in enumerate(all_dyes):
+        res_5p.append(residues[dye.index-1])
+        res_3p.append(residues[dye.index+1])
+
+        #Calculate complementary basepairs
+        n_bp = len([r for r in chains[0].residues])
+        opp_bp = lambda x: (2*n_bp - x) - 1 #Calculate index of complementary bp
+        res_5p_opp.append(residues[opp_bp(res_5p[-1].index)])
+        res_3p_opp.append(residues[opp_bp(res_3p[-1].index)])
+        
+    #Calculate dye orientation vectors in real space
+    dye_vects = [calc_dye_vects(xyz,r) for r in all_dyes]
+
+    #t1 vector is the average between the cross-strand sugar axis in the
+    #N+1 and N-1 planes, minus its projection onto t3
+    #t3 vector is the average normal to the N+1 and N-1 planes, pointing
+    #3' to 5'
+    t1_vects,t3_vects = calc_t1_t3_vects(xyz,res_5p,res_3p,res_5p_opp,res_3p_opp)
+
+    print("T1 and t3 vects calculated.")
+    print("Calculating T2 vect:")
+
+    #t2 = t3 cross t1
+    if type(t1_vects) != list:
+        t1_vects = [t1_vects]
+        t3_vects = [t3_vects]
+    t2_vects = [np.cross(t3_vects[i],t1_vects[i]) for i in range(len(t1_vects))]
+
+    renorm_2d = lambda x: x / (np.linalg.norm(x,axis=1)[:,np.newaxis])
+    
+    t_vects = []
+    #Calculate components in the Darboux frame
+    for i, dv in enumerate(dye_vects):
+        tv = np.zeros((len(dv),3), dtype='float')
+        tv[:,0] = np.sum(t1_vects[i]*dv, axis=-1)
+        tv[:,1] = np.sum(t2_vects[i]*dv, axis=-1)
+        tv[:,2] = np.sum(t3_vects[i]*dv, axis=-1)
+        #Renormalize
+        tv = renorm_2d(tv)
+        t_vects.append(np.copy(tv)) 
+        
+    return t_vects, dye_names
+    
+def calc_dye_vects(xyz, res):
+    """Calculate the real-space orientation vector of the dye res, 5' to 3'
+
+    *Parameters*
+            xyz:  *np.array of floats*, shape: (n_ts,n_atoms,3)
+                    Atomic positions.
+            res: *mdtraj.residue*
+                    Residue of the dye
+    *Returns*
+            dye_vects: *list of np.array*, shape: [(n_ts,3)]
+                    Components of the dye's orientation in the xyz frame.
+    """
+    
+    #Determine the residues of dyes
+    indole_5p_inds = []
+    indole_3p_inds = []
+
+    #Determine name of all atoms in dye
+    if res.name == "C5N":
+        indole_5p_names = set(['N2', 'C15', 'C14','C13','C12','C17','C16',
+                'C18','C19','C20','C30'])
+        indole_3p_names = set(['N1','C1','C2','C3','C4','C5','C6','C7','C29',
+                'C28','C8'])
+    elif res.name == "C3N":
+        indole_5p_names = set(['N2', 'C15', 'C14','C13','C12','C17','C16',
+                'C18','C19','C20','C30'])
+        indole_3p_names = set(['N1','C1','C2','C3','C4','C5','C6','C7','C29',
+                'C28','C8'])
+    else:
+        raise ValueError("Incorrect name of dye residue: %s" % res.name)
+    
+    indole_5p_inds = np.array([at.index for at in res.atoms if at.name in indole_5p_names])
+    indole_3p_inds = np.array([at.index for at in res.atoms if at.name in indole_3p_names])
+
+    #Calculate COMs
+    indole_5p_com = np.average(xyz[:,indole_5p_inds],axis=1)
+    indole_3p_com = np.average(xyz[:,indole_3p_inds],axis=1)
+
+    dye_vects = indole_3p_com - indole_5p_com
+    #Normalize
+    print("Dye_vects shape:",dye_vects.shape)
+    dye_vects = np.transpose(np.transpose(dye_vects) / 
+            np.linalg.norm(dye_vects,axis=1) )
+
+    return dye_vects
+
+def calc_t1_t3_vects(xyz, res_5p,res_3p,res_5p_opp,res_3p_opp):
+    """Calculate the real-space orientation vector t3, the principal tangent.
+    t3 vector is the average normal to the N+1 and N-1 planes.
+
+    *Parameters*
+            xyz:  *np.array of floats*, shape: (n_ts,n_atoms,3)
+                    Atomic positions.
+            res_5p: *mdtraj.residue or list of residues*
+                    Residue of DNA in the same-strand 5'-ward of the dye.
+            res_3p: *mdtraj.residue or list of residues*
+                    Residue of DNA in the same-strand 3'-ward of the dye.
+            res_5p_opp: *mdtraj.residue or list of residues*
+                    Residue of DNA in the opposite-strand complementary
+                    to res_5p.
+            res_3p_opp: *mdtraj.residue or list of residues*
+                    Residue of DNA in the opposite-strand complementary
+                    to res_3p.
+    *Returns*
+            t1_vects: *list of np.array*, shape: [n_dyes,(n_ts,3)]
+                    Normalized components of the t1 orientation in the xyz frame.
+            t3_vects: *list of np.array*, shape: [n_dyes,(n_ts,3)]
+                    Normalized components of the t3 orientation in the xyz frame.
+    """
+    #If called on a strand with multiple dyes, call recursively
+    if type(res_5p) == list:
+        t1_vects = []
+        t3_vects = []
+        for i in range(len(res_5p)):
+            t1v, t3v = calc_t1_t3_vects(xyz,res_5p[i],res_3p[i],res_5p_opp[i],
+                    res_3p_opp[i])
+            t1_vects.append(np.copy(t1v))
+            t3_vects.append(np.copy(t3v))
+        return t1_vects, t3_vects
+
+
+
+    #Determine the indices to calculate COMs for each of the corresponding
+    #Residues
+    indole_5p_inds = []
+    indole_3p_inds = []
+
+    #Determine name of all atoms in dye
+    vect_dict = {} #Key: Resname. Value: "startpoint atom name set", "
+    #                  endpoint atom name set" 
+    
+    #Set atom names for every possible residue name
+    vect_dict["C5N"] = [set(['N2', 'C15', 'C14','C13','C12','C17','C16',  
+                'C18','C19','C20','C30']),set(['N1','C1','C2','C3','C4','C5','C6','C7','C29',
+                'C28','C8'])]
+    vect_dict["C3N"] = [set(['N2', 'C15', 'C14','C13','C12','C17','C16',  
+                'C18','C19','C20','C30']),set(['N1','C1','C2','C3','C4','C5','C6','C7','C29',
+                'C28','C8'])]
+    
+    vect_dict["DG"] = [set(["C4'","O4'","C1'","C2'","C3'"]),set(['N1','C2','N2','N3','C4','C5',
+                    'C6','O6','N7','C8','N9'])]
+    vect_dict["DC"] = [set(["C4'","O4'","C1'","C2'","C3'"]),set(['N1','C2',
+                    'O2','N3','C4','N4','C5','C6'])]
+    vect_dict["DA"] = [set(["C4'","O4'","C1'","C2'","C3'"]),set(['N1','C2','N3','C4','C5',
+                    'C6','N6','N7','C8','N9'])]
+    vect_dict["DT"] = [set(["C4'","O4'","C1'","C2'","C3'"]),set(['N1','C2',
+                    'O2','N3','C4','O4','C5','C6','C7'])]
+
+    #Replace residues with atom indices
+    res_labels = ["res_5p","res_3p","res_5p_opp","res_3p_opp"]
+    all_res = [res_5p,res_3p,res_5p_opp,res_3p_opp]
+    all_start_inds = [] #Shape: 4, n_atoms
+    all_end_inds = [] #Shape: 4,n_atoms
+    for i, res in enumerate(all_res):
+        all_start_inds.append(np.array([at.index for at in res.atoms if at.name in vect_dict[res.name][0]]))
+        all_end_inds.append(np.array([at.index for at in res.atoms if at.name in vect_dict[res.name][1]]))
+
+
+    #Calculate COMs
+    all_start_coms = np.array([np.average(xyz[:,start_inds],axis=1) for start_inds in all_start_inds])
+    all_end_coms = np.array([np.average(xyz[:,end_inds],axis=1) for end_inds in all_end_inds])
+
+    #Calculate explicit orientation vectors
+    dye_vects = all_end_coms - all_start_coms
+    print("Dye_vects shape, should be (4,n_ts,3):",dye_vects.shape)
+
+    #Split into individual variables
+    vect_5p = dye_vects[0]
+    vect_3p = dye_vects[1]
+    vect_5p_opp = dye_vects[2]
+    vect_3p_opp = dye_vects[3]
+
+    renorm_2d = lambda x: x / (np.linalg.norm(x,axis=1)[:,np.newaxis])
+    renorm_3d = lambda x: x / (np.linalg.norm(x,axis=2)[:,:,np.newaxis])
+
+    print("vect_5p shape:",vect_5p.shape)
+
+    #Normalize each vector
+    vect_5p = renorm_2d(vect_5p)
+    vect_3p = renorm_2d(vect_3p)
+    vect_5p_opp = renorm_2d(vect_5p_opp)
+    vect_3p_opp = renorm_2d(vect_3p_opp)
+
+    #These vectors should be roughly negative of each other. We can
+    #Take an appropriate average by subtracting them and renormalizing!
+
+    print("Finished renormalizing. Subtracting...")
+    t1_5p = vect_5p - vect_5p_opp
+    t1_5p = renorm_2d(t1_5p)
+    t1_3p = vect_3p - vect_3p_opp
+    t1_3p = renorm_2d(t1_3p)
+
+    t1_vects = np.average((t1_5p,t1_3p),axis=0)
+
+    print("Performing cross product...")
+    #We do the same thing with a crossproduct to calculate t3.
+    t3_5p = np.cross(vect_5p,vect_5p_opp)
+    t3_3p = np.cross(vect_3p,vect_3p_opp)
+    #These should point in the same direction. Renormalize, average, then
+    #renormalize.
+    t3_5p = renorm_2d(t3_5p)
+    t3_3p = renorm_2d(t3_3p)
+    
+    print("Removing projection...")
+    t3_vects = renorm_2d(t3_5p + t3_3p)
+    #Remove projection of t3 onto t1
+    t3_vects = t3_vects - t1_vects * np.sum(t3_vects * t1_vects,axis=-1)[:,np.newaxis]
+    t3_vects = renorm_2d(t3_vects)
+    print("returning.")
+    return t1_vects, t3_vects
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-d",
+        "--dir",
+        type=str,
+        default=".",
+        help="Path to simulation Processing directory.",
+    )
+    # parser.add_argument('-f','--file',type=str,help="Path to lammpstrj to analyze.")
+    args = parser.parse_args()
+
+    main()
+
---
+++ plot_darboux.py
@@ -0,0 +1,296 @@
+#Module plot_darboux
+#Created by Aria Coraor
+#Written 9/13/24
+
+import numpy as np
+import argparse
+import matplotlib
+matplotlib.use("Agg")
+from matplotlib import rc
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import pandas as pd
+import os
+import zfit
+import KDEpy
+import matplotlib.transforms as transforms
+import cartopy.crs as ccrs
+from scipy.interpolate import RegularGridInterpolator as RGI
+
+def main():
+    """Plot each cv versus time"""
+    #Load pandas dataframe
+    df = pd.read_csv('log.dframe')
+
+    #Find columns and plot
+    cols = df.columns
+    darb_cols = [col for col in cols if col[-3:] in set(["_t1","_t2","_t3"])]
+    darboux_plot(df,darb_cols)
+
+        
+    print("Created globe plot. Exiting...")
+
+
+def darboux_plot(df,names):
+    '''Create a 2d-mapped plot of the probability density of the Darboux
+    vector using Cartopy.
+
+    Parameters:
+        df: *pd.Dataframe*
+                Full log data.
+        names: *list of str*
+            Names of all Darboux columns
+    '''
+
+    #Extract the names and number of each dye
+    dye_names = list(set([name[:-3] for name in names]))
+    dye_names.sort()
+
+    #Iterate over each separate dye name and create plot
+    for i, dye in enumerate(dye_names):
+
+        #Calculate phi, theta from t1 / t2 / t3 components
+        t1 = df["%s_t1" % dye].to_numpy()
+        t2 = df["%s_t2" % dye].to_numpy()
+        t3 = df["%s_t3" % dye].to_numpy()
+        phis = np.arccos(t3) # Azimuthal angle, radians, 0-Pi
+        thetas = np.arctan2(t2,t1) #Longitudinal angle, 0-2Pi
+        
+        #Transform to plate carree, degrees
+        thetas *= 180.0/np.pi
+        phis = 90 - (phis*180.0/np.pi)
+
+        print("Theta range:",[np.min(thetas),np.max(thetas)])
+        print("Phis range:",[np.min(phis),np.max(phis)])
+
+
+        #Create numpy histogram for pcolormesh
+        xedges = np.linspace(-180.0,180.0,91)
+        yedges = np.linspace(-90.0,90.0,91)
+        xcent = (xedges[:-1] + xedges[1:])/2
+        ycent = (yedges[:-1] + yedges[1:])/2
+        #If 2d KDEs are working, use that:
+        try:
+            H, xed, yed = calc_darboux_kde(t1,t2,t3,name=dye)
+        except Exception as e:
+            print("Failed in kde:",e)
+            
+            print("Continuing with standard histogram.")
+            H, xed, yed = np.histogram2d(thetas,phis,bins=[xedges,yedges])
+        
+        #Finally, plot:
+        plt.clf()
+        fig = plt.figure()
+        gs = fig.add_gridspec(2,2,width_ratios = (4,1),height_ratios = (1,4))
+        ax1 = fig.add_subplot(gs[0,0])
+        ax2 = fig.add_subplot(gs[0,1])
+        ax3 = fig.add_subplot(gs[1,0],projection=ccrs.EckertIV())
+        ax4 = fig.add_subplot(gs[1,1])
+        '''
+        fig, axs = plt.subplots(
+            nrows=2,
+            ncols=2,
+            sharex="col",
+            sharey="row",
+            layout="constrained",
+            width_ratios=(4, 1),
+            height_ratios=(1, 4),
+            subplot_kw = {'projection': 'rectilinear'}
+        )
+        ax1 = axs[0, 0]
+        ax2 = axs[0, 1]
+        ax3 = axs[1, 0]
+        ax4 = axs[1, 1]
+        '''
+        ax2.set_visible(False)
+        # Define matplotlib transforms
+        base = ax4.transData
+        scale = transforms.Affine2D().scale(sx=-1, sy=1)
+        rot = scale.rotate_deg(270)
+        
+        #ax3 = 
+        #ax3.projection=ccrs.EckertIV()
+        ax3.pcolormesh(xcent,ycent,H,cmap='viridis',shading='auto',
+            transform=ccrs.PlateCarree())
+        ax3.gridlines(linestyle=':')
+        fig.savefig('%s_darboux.png'% dye,dpi=600)
+        fig.savefig('%s_darboux.svg'% dye)
+        fig.savefig('%s_darboux.pdf'% dye)
+        #Pasted from joined_pca:
+
+def calc_darboux_kde(t1,t2,t3,gridpoints=512,name='darboux_scat'):
+    '''Create a proper KDE of the darboux vectors in PlateCarRee coordinates,
+
+    Parameters:
+        t1: *np.array*, shape: (n_ts)
+                Components of the orientation in the t1 direction (Stiffest).
+        t2: *np.array*, shape: (n_ts)
+                Components of the orientation in the t2 direction. (Floppy)
+        t3: *np.array*, shape: (n_ts)
+                Components of the orientation in the t3 direction. (Principal)
+        gridpoints: *int*
+            Number of gridpoints for the evaluation of the KDE.
+        name: *str*
+            Name for saving 2d plot.
+
+    Returns:
+        H: *2d np.array*, shape: (gridpoints,gridpoints)
+                KDE density in theta-phi space.
+        xcent: *np.array*, shape: (gridpoints)
+                Center of 'histogram' bins for kde density evaluations, 
+                in longitudinal degrees.
+        ycent: *np.array*, shape: (gridpoints)
+                Center of 'histogram' bins for kde density evaluations, 
+                in latitudinal degrees.
+    '''
+    #Perform the KDE in Darboux space, then map the pdf to PlateCaree space,
+        #Then project onto EckertIV space
+    #phi = zfit.Space("Phi", lower = -90, upper = 90)
+    #theta = zfit.Space("Theta", lower = -180, upper = 180)
+    t1_obs = zfit.Space("t1", lower = -1, upper = 1)
+    t2_obs = zfit.Space("Theta", lower = -1, upper = 1)
+    t3_obs = zfit.Space("Theta", lower = -1, upper = 1)
+
+    print("Fitting kdes...")
+    #phi_data = zfit.Data(phi,obs=phi
+    kde_t1 = zfit.pdf.KDE1DimISJ(t1, obs=t1_obs)
+    kde_t2 = zfit.pdf.KDE1DimISJ(t2, obs=t2_obs)
+    kde_t3 = zfit.pdf.KDE1DimISJ(t3, obs=t3_obs)
+    #kde_phi = zfit.pdf.KDE1DimISJ(phi, obs=phi)
+    #kde_theta = zfit.pdf.KDE1DimISJ(theta, obs=theta)
+    print("KDEs fit.")
+    #H = kde_eval()
+            
+
+    print("Performing fft kdes.")
+    #Calculate geometric mean bandwidth
+    geom_mean_bandwidth = (float(kde_t1._bandwidth.numpy()) *
+            float(kde_t2._bandwidth.numpy())*
+            float(kde_t3._bandwidth.numpy()))**(1/3)
+    
+
+    #Convert joined_pca code into 3d KDE
+    fft_kde = KDEpy.FFTKDE(kernel='gaussian',
+            bw=geom_mean_bandwidth)
+    data_all = np.column_stack((t1.ravel(),t2.ravel(),t3.ravel()))
+    fft_kde.fit(data_all)
+    
+    #Calculate density for 1v2 and 1v3
+    fft_points = gridpoints
+    gridpoints, density = fft_kde.evaluate(fft_points)
+    density = density / np.max(density) # normalize
+
+    #Convert to RGI formatting
+    grid_x = np.sort(np.unique(gridpoints[:,0]))
+    grid_y = np.sort(np.unique(gridpoints[:,1]))
+    grid_z = np.sort(np.unique(gridpoints[:,2]))
+    density_mesh = np.reshape(density,(fft_points,fft_points,fft_points))
+    print("grid_x.shape:",grid_x.shape)
+
+    #Density mesh: normalized pdf in t1, t2, t3 space!
+    
+    #Calculate scatter color for all points in data
+    interpolator = RGI((grid_x,grid_y,grid_z),density_mesh)
+    colors = interpolator(data_all)
+    #colors_1v3 = interpolator_1v3(data_1v3)
+
+    #Now: from density_mesh, calculate phi and theta!
+    #phi_theta_mesh = np.zeros(shape=(fft_points,fft_points))
+    #Using formulas for the pdf transformation from <https://tinyurl.com/bdjkpjsk>
+    phis = np.arccos(data_all[:,2]) # Azimuthal angle, radians, 0-Pi
+    thetas = np.arctan2(data_all[:,1],data_all[:,0]) #Longitudinal angle, 0-2Pi
+    
+    #Transform to plate carree, degrees
+    thetas *= 180.0/np.pi
+    phis = 90 - (phis*180.0/np.pi)
+
+    #Create scatterplot on ax3
+    plt.clf()
+    fig = plt.figure()
+    gs = fig.add_gridspec(7,2,width_ratios = (4,1),
+        height_ratios = (6,4,4,4,4,4,4))
+    ax1 = fig.add_subplot(gs[0,0])
+    ax2 = fig.add_subplot(gs[0,1])
+    ax3 = fig.add_subplot(gs[1:,0],projection=ccrs.EckertIV())
+    ax4 = fig.add_subplot(gs[2:-1,1])
+
+    ax2.set_visible(False)
+    base = ax4.transData
+    scale = transforms.Affine2D().scale(sx=-1, sy=1)
+    rot = scale.rotate_deg(270)
+    cmap = cm.viridis
+    
+    #Sort so that brightest colors are scattered last
+    color_shape = colors.shape
+    color_sorter = np.argsort(colors.ravel()).ravel()
+    colors_sorted = colors.ravel()[color_sorter]
+    thetas_sorted = thetas.ravel()[color_sorter]
+    phis_sorted = phis.ravel()[color_sorter]
+
+    ax3.scatter(thetas_sorted,phis_sorted, marker=".", s=1, c=cmap(colors_sorted),
+        transform=ccrs.PlateCarree())
+    ax3.gridlines(linestyle=':')
+    #ax3.set_xlabel("PC 1")
+    #ax3.set_ylabel("PC 2")
+    
+    #bounds_1v2 = (-4.0,7.0)
+    #bounds_1v3 = (-3.0, 4.0)
+    ax3.set_extent([-180,180,-90,90])
+    #ax3.set_ylim(-90,90)
+    #ax3.set_xlim(-180,180)
+    
+    #Plot other kdes
+    phi_sp = zfit.Space("Phi", lower = -90, upper = 90)
+    theta_sp = zfit.Space("Theta", lower = -180, upper = 180)
+    kde_phi = zfit.pdf.KDE1DimISJ(phis, obs=phi_sp) #True azimuthal angle
+    kde_theta = zfit.pdf.KDE1DimISJ(thetas, obs=theta_sp)
+    #xlim = ax3.get_xlim()
+    #ylim = ax3.get_ylim()
+    x = np.linspace(-180.0,180.0,1000)
+    y_eval = np.linspace(-90.0,90.0,1000)
+    kde_horiz = kde_theta.pdf(x)
+    kde_vert = kde_phi.pdf(y_eval)
+    y_plot = np.linspace(0.0,180.0,1000)
+
+    lw = 0.5
+    color = '#0072B2'
+    ax1.plot(x, kde_horiz, linewidth=lw, color=color)
+    ax1.fill_between(x, kde_horiz, color=color, alpha=0.25)
+    ax1.set_xlim((-180.0,180.0))
+    theta_labels = ["-180","-120","-60","0","60","120","180"]
+    ax1.set_xticks([float(ele) for ele in theta_labels])
+    ax1.set_xticklabels(theta_labels)
+    #ax4.set_xlim((0.0,180.0))
+    ax4.plot(y_plot, kde_vert, linewidth=lw, color=color, 
+        transform = rot+base)
+    xlim = ax4.get_xlim()
+    ax4.fill_between(y_plot, kde_vert, color=color, alpha=0.25, 
+            transform  = rot+base)
+    ax4.set_xlim(xlim)
+    ax4.set_ylim(0.0,180.0)
+    phi_labels = ["180","150","120","90","60","30","0"]
+    print("xlim:",xlim)
+    print("ylim:",ax4.get_ylim())
+    ax4.set_yticks([float(ele) for ele in phi_labels])
+    ax4.set_yticklabels([ele for ele in reversed(phi_labels)])
+    #ax4.set_xlim(0,np.max(kde_vert)*1.1)
+
+    
+    base = name + "_scatt"
+    plt.savefig(base + ".png", dpi=600)
+    plt.savefig(base + ".pdf")
+    plt.savefig(base + ".svg")
+    plt.savefig(base + ".eps")
+    print("Saved %s.png" % base)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f','--file', type=str, default='output.h5',
+        help='Path to ABF integrated output')
+    parser.add_argument('-m','--max', type=float, default=40.0,
+        help='Maximum free energy to plot up to. Default = 40.0.')
+    args = parser.parse_args()
+
+    main()
+
